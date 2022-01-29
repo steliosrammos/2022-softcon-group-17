69351c4fa1c6221e9a1200f8ebc7c8a147b1c5eb
Andrei Bolos <andreibolos16@gmail.com>
2022-01-29 19:23:49 +0200

    structure, get api and dockerfile

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function App() {
+
+  let  [meals, setMeals] = useState(null);
+
+  useEffect(() => {
+    //Temorary until we fix API issue
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => setMeals(data.message))
+      .catch((error) => {
+        console.log('Failed to retrieve meals.', error);
+      })
+  }, []);
+
+
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = (data) => {
 
     // Temporary to test out the api
     const formatedData = {
-      fname: data.Name,
-      lname:  data.Email
+      meal: data.Meal,
+      qty:  data.Qty
     }
 
     const requestOptions = {
@@ -17,7 +31,7 @@ export default function App() {
         body: JSON.stringify(formatedData)
     };
 
-    fetch("http://127.0.0.1:5000/api/people", requestOptions)
+    fetch("http://127.0.0.1:5000/api/order", requestOptions)
       .then(response => response.json())
       .then(res => console.log(res))
       .catch((error) => {
@@ -26,26 +40,87 @@ export default function App() {
   }
   console.log(errors);
   return (
-    <section>
-    <div className="title">BOOKLET.</div>
-    <div className="text-form">Are you looking for a book? Get inspired.</div>
+    <section className="App">
+    <div className="title">Bistro Delivery</div>
+    <div className="text-form">What is your order?</div>
 
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Name" {...register("Name", {required: true, maxLength: 100})} />
-      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-      <select {...register("Choice")}>
-        <option value="Action&Adventure">Action&Adventure</option>
-        <option value="Fantasy"> Fantasy</option>
-        <option value=" Classics"> Classics</option>
-        <option value=" Detective&Mistery"> Detective&Mistery</option>
-        <option value=" Historical Fiction"> Historical Fiction</option>
-        <option value=" Romance"> Romance</option>
-        <option value=" Science Fiction (Sci-Fi)"> Science Fiction (Sci-Fi)</option>
-        <option value=" Suspense&Thrillers"> Suspense&Thrillers</option>
-        <option value=" Biographies and Autobiographies"> Biographies and Autobiographies</option>
-      </select>
 
-      <input type="submit" value="Send"/>
+    <table>
+      <tbody>
+        <tr>
+          <th>Meal No. </th>
+          <th>Description </th>
+          <th>Price</th>
+          <th>Qty Avail.</th>
+          <th>Order </th>
+        </tr>
+
+    		<tr>
+    			<td>1</td>
+    			<td>Spicy Cabbage Soup</td>
+    			<td>12$ </td>
+    			<td>3 </td>
+    			<td>
+          <select {...register("Choice_1")}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+          </select>
+          </td>
+    		</tr>
+    		<tr>
+    			<td>2</td>
+    			<td>Calamari with Mushrooms</td>
+    			<td>15$ </td>
+    			<td>7 </td>
+    			<td>
+          <select {...register("Choice_2")}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+          </select>
+          </td>
+    		</tr>
+    		<tr>
+    			<td>3</td>
+    			<td>Beef with Broccoli</td>
+    			<td>24$</td>
+    			<td>2 </td>
+    			<td>
+          <select {...register("Choice_3")}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+          </select>
+          </td>
+    		</tr>
+      </tbody>
+    </table>
+
+      <input type="submit" value="Edit order"/>
+      <input type="submit" value="Send order"/>
     </form>
 
     <div className="footer">
