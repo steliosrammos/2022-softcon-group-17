5cb646ef020cfeb468edabfc0a464ead225e44de
Ringo Groenewegen <ringogroenewegen@gmail.com>
2022-02-01 17:24:20 +0100

    db ip and password used from secret

--- api/src/config.py
+++ api/src/config.py
@@ -16,7 +16,21 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 # Build the Sqlite ULR for SqlAlchemy
 #db_url = "sqlite:///" + os.path.join(basedir, "bistro.db")
 # db_url = 'postgresql://postgresadmin:admin123@10.152.183.6:5432/postgresdb'
-db_url = 'postgresql://postgresadmin:admin123@10.152.183.126:5432/postgresdb'
+#db_url = 'postgresql://postgresadmin:admin123@10.152.183.126:5432/postgresdb'
+
+try:
+    POSTGRES_SERVICE = os.environ['POSTGRES_SERVICE_PORT_5432_TCP_ADDR']
+    print('Using environment variable to resolve DB service')
+except:
+    POSTGRES_SERVICE = '10.152.183.234'
+    print('Using IP address for DB service')
+
+
+# Get password from secret volume
+with open('/etc/db_pwd/POSTGRES_PASSWORD', 'r') as f:
+    db_pwd = f.read().strip()
+
+db_url = f'postgresql://postgresadmin:{db_pwd}@{POSTGRES_SERVICE}:5432/postgresdb'
 
 # Configure the SqlAlchemy part of the app instance
 app.config["SQLALCHEMY_ECHO"] = True
