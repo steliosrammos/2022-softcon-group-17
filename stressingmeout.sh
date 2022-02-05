3bcae239d4babbefc2a43e76e757faaa5b1b5aba
Stelios Rammos <stelio.rammos@gmail.com>
2022-02-05 15:46:57 +0100

    configure hpa

--- stressingmeout.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-microk8s kubectl run -i --tty load-generator --rm --image=busybox --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -qO- https://10.0.2.15:443/api/orders --no-check-certificate; done"
