ec264d33074f2509ed3584c8da34051849b55016
Stelios Rammos <stelio.rammos@gmail.com>
2022-02-06 22:19:39 +0100

    changes to hpa and canary

--- kubernetes/stressingmeout.sh
+++ kubernetes/stressingmeout.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 HOST_IP=$(ip -4 addr show enp0s5 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-echo $HOST_IP
-microk8s kubectl run -i --tty load-generator --rm --image=busybox --restart=Never --env="HOST_IP=$HOST_IP" -- /bin/sh -c "while sleep 0.001; do wget -qO- https://$HOST_IP:443/api/orders --no-check-certificate > /dev/null; done"
+microk8s kubectl run -i load-generator --rm --image=busybox --restart=Never --env="HOST_IP=$HOST_IP" -- /bin/sh -c "while sleep 0.001; do wget -qO- https://$HOST_IP:443/api/orders --no-check-certificate > /dev/null; done" &
